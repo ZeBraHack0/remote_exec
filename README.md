13671c7b396d164e7f8887d9ffbb7324f44a9961
ZeBraHack0 <git@github.com>
2023-08-31 13:42:51 +0800

    add docker exec

--- README.md
+++ README.md
@@ -3,11 +3,38 @@
 execute and kill programs on multiple servers concurrently and automatically
 
 ### start a new program
-```
+```shell
 bash sync_exec.sh start
 ```
 
 ### kill a program: modify the keyword variable in the script
-```
+```shell
 bash sync_exec.sh stop
 ```
+
+
+
+### before executing a sudo program
+
+execute sudo commands on the server without typing passwords：
+
+```shell
+sudo vi /etc/sudoers
+```
+
+add the following line：
+
+```shell
+username    ALL=(ALL:ALL) NOPASSWD:ALL
+```
+
+
+
+### adjust script execution path
+
+add the following line in the begin of your own script
+
+```shell
+cd $(dirname $0)
+```
+
--- sync_exec.sh
+++ sync_exec.sh
@@ -8,11 +8,12 @@ source ./include/YCNic.sh
 
 mypasswd=""
 myuser="" 
+mydocker=""
 
 PORT=22
 SSH_LIST=(
-    # hop_format: username@ip port, e.g., root@192.168.0.1 22
-    "${myuser}@192.168.211.109 ${PORT} "
+    # hop_format: username@ip port container_name, e.g., root@192.168.0.1 22 node_1
+    "${myuser}@192.168.211.109 ${PORT} ${mydocker}"
 )
 
 sync_start() {
@@ -24,22 +25,38 @@ sync_start() {
     done
 }
 
+docker_start() {
+    # example: cmd2="bash megatron/examples/pretrain_gpt_distributed_small.sh"
+    for ssh_item in "${SSH_LIST[@]}"; do
+        local ssh_para=(${ssh_item})
+        cmd1="docker exec -i ${ssh_para[2]} "
+        cmd2=""
+        echo_back "sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} ${cmd1} ${cmd2} > ${ssh_para[0]}.log 2>&1 &"
+    done
+}
+
 sync_stop() {
     echo_info "Terminate program..."
     key_word=""
-    # cmd="ps -aux |grep ${key_word} |grep -v grep|awk '{print '$2'}'"
-    # cmd="sudo kill -9 `pgrep ${key_word}`"
     cmd="ps -ef|grep ${key_word}|grep -v grep|cut -c 9-16"
     for ssh_item in "${SSH_LIST[@]}"; do
         local ssh_para=(${ssh_item})
-        echo_info "sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} $cmd"
-        local mypids=`sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} $cmd`
+        echo_back "sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} $cmd"
+        local mypids=(`sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} $cmd`)
         for mypid in "${mypids[@]}"; do
             echo_info "kill process ${mypid}"
             echo_back "sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} sudo kill -9 ${mypid}"
-            # echo_back "sshpass -p ${mypasswd} ssh ${ssh_para[0]} -p ${ssh_para[1]} $cmd"
         done
     done
+}
+
+show_usage() {
+    appname=$0
+    echo_info "Usage: ${appname} [command], e.g., ${appname} start"
+    echo_info "  start"
+    echo_info "  docker"
+    echo_info "  stop"
+    echo_info "  -- help                          show help message"
 }
 
 ################################################################
@@ -59,6 +76,9 @@ global_choice=${1}
 case ${global_choice} in
     "start")
         sync_start
+        ;;
+    "docker")
+        docker_start
         ;;
     "stop")
         sync_stop
